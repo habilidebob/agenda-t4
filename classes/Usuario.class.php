682be4a95fbd9dac7b3c1a6bb399076dc9a122b7
Habilidebob de Jesus <80491104+habilidebob@users.noreply.github.com>
2023-05-16 21:53:10 -0300

    login

--- classes/Usuario.class.php
+++ classes/Usuario.class.php
@@ -18,7 +18,16 @@ class Usuario{
         Banco::desconectar();
     }
     public function Logar(){
-
+        // Copiei do Listar()
+        $banco = Banco::conectar();
+        $sql = "SELECT * FROM usuarios WHERE email = ? AND senha = ?";
+        $comando = $banco->prepare($sql);
+        $hash = hash('sha256', $this->senha);
+        $comando->execute(array($this->email,$hash));
+        // "Salvar" o resultado da consulta (tabela) na $resultado
+        $resultado = $comando->fetchAll(PDO::FETCH_ASSOC);
+        Banco::desconectar();
+        return $resultado;
     }
 }
 
